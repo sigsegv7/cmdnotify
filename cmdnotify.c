726efd9ede85f4f5bc7c7629946326d1422b3ea5
sigsegv7 <ian@vegaa.systems>
2023-09-25 06:57:46 -0400

    docs: Update LICENSE
    Signed-off-by: sigsegv7 <ian@vegaa.systems>

--- cmdnotify.c
+++ cmdnotify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Emilia Strange.
+ * Copyright (c) 2023 Ian Marco Moffett.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
