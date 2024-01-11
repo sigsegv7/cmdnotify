2099641eba8b6d5ab0f738a3136621b0e99985ac
Ian Moffett <ian@osmora.org>
2024-01-11 01:13:24 -0500

    Update copyright
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- cmdnotify.c
+++ cmdnotify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Ian Marco Moffett.
+ * Copyright (c) 2023-2024 Ian Marco Moffett.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
